822d6c0ee18e9fcf852da40d88399de058492b97
jacob-harrison <jrhmtf@mst.edu>
2018-09-20 21:52:26 -0500

    Added network scanner

--- java/PlantCam/src/Client.java
+++ java/PlantCam/src/Client.java
@@ -4,18 +4,35 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		String serverAddress = JOptionPane.showInputDialog(
-				"Enter IP Address of machine that has server:");
+		scanNetwork("192.168.2");
+		Scanner scanny = new Scanner(System.in);
+		System.out.print("Enter address you would like to connect to: ");
+		String serverAddress = scanny.nextLine();
 		Socket s = new Socket(serverAddress, 9090);
 		BufferedReader input = 
 				new BufferedReader(new InputStreamReader(s.getInputStream()));
 		String answer = input.readLine();
-		JOptionPane.showMessageDialog(null,  answer, "Response", 0);
+		System.out.println(answer);
+		scanny.close();
 		System.exit(0);
+	}
+
+	public static void scanNetwork(String baseNetwork) throws IOException{
+		int timeout=10;
+		for(int i = 1; i < 20; i++){
+			String host = baseNetwork + "." + i;
+			if(InetAddress.getByName(host).isReachable(timeout)){
+				System.out.println("Found device:" + host);
+			}
+			System.out.print("\rScanning network: ["+ (i * 100 / 20) + "%]");
+		}
+		System.out.println();
 	}
 }
