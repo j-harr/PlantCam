a2c0bc1c3d4f8d2e0263189c6a70a3c0bcb14d65
Jacob Harrison <jrhmtf@mst.edu>
2018-09-23 12:51:56 -0500

    Added test class

--- java/PlantCam/src/Master.java
+++ java/PlantCam/src/Master.java
@@ -1,22 +1,30 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
 import java.util.Scanner;
 
 public class Master {
+	private List<Device> devices;
+	private DeviceGroup deviceGroup;
+	
 	public void execute() throws Exception {
 		System.out.println("Executing as MASTER");
 		System.out.println("Type 'exit' to quit");
 		FindDevices deviceFinder = new FindDevices(9030);
-		deviceFinder.call();
+		
+		devices = deviceFinder.call();
+		deviceGroup = new DeviceGroup(devices);
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Starting loop in Master");
 		while(true) {
 			String line = scan.nextLine();
 			if(line.equalsIgnoreCase("EXIT"))
 				break;
-			String host = line.substring(0, line.indexOf("=>"));
-			String message = line.substring(line.indexOf("=>"), line.length());
+			else {
+				deviceGroup.send(line);
+			}
 			
 		}
 		scan.close();
