7bf5fa3a41989398675984422874c7306afa808f
Jacob Harrison <jrhmtf@mst.edu>
2018-10-09 22:07:57 -0500

    Compiled them

---
+++ testing/stream.cpp
@@ -0,0 +1,34 @@
+/*
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+*/
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+using boost::asio::ip::address;
+
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include "videoStream.hpp"
+
+int main(int argc, char* argv[]){
+    videoStream vStream("127.0.0.1", 9090);
+    vStream.start();
+
+    std::string input;
+    while(std::cin >> input){
+        if(input == "quit"){
+            break;
+            vStream.stop();
+        }
+        else if(input == "stop"){
+            vStream.stop();
+        }
+        else
+            std::cout << "Continuing" << std::endl;
+    }
+    return 0;
+}
